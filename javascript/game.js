e614110a2f63f574ce2d405ed1447944f8dfae76
Joseph V <joseph.vonedwins@yahoo.com>
2018-06-02 20:28:20 -0400

    check letter correctness

--- javascript/game.js
+++ javascript/game.js
@@ -6,6 +6,10 @@
 
 var wordList = ["cat", "dog", "monkey"];
 var selectedWord = 'hangman';
+var guessesRemaining = 5;
+var wrongGuesses = [];
+var correctGuesses = [];
+var guesses = [];
 
 
 //=================================
@@ -14,11 +18,32 @@ var selectedWord = 'hangman';
 
 //Function that selects a random word from the list
  function selectWord() {
-    var selectedWord = wordList[Math.floor(Math.random() * wordList.length)];   
+    selectedWord = wordList[Math.floor(Math.random() * wordList.length)];   
+    console.log(selectedWord);
 }
 
+function checkGuess(keyPress) {
+    //Checking if letter has baeen guessed before
+        
+//=======================
+    //Checking guesses
+//=======================
+
+    //Check if letter has already been guessed
+
+    //See if letter is correct
+        for (i = 0; i < selectedWord.length; i++)
+
+            if (selectedWord[i] === keyPress.key) {
+               correctGuesses.push(keyPress.key);
+               console.log(correctGuesses);
+            };
+    }
 
 
-//================================
-//Call functions in order section
-//================================
+        //===================
+        //Call Functions Here
+        //===================
+selectWord();
+
+addEventListener("keyup", checkGuess)
