a11bd90b70901610fb33a98f4aa1e50b0100eeac
Joseph V <joseph.vonedwins@yahoo.com>
2018-06-03 14:32:45 -0400

    completed letter check and score decreaser

--- javascript/game.js
+++ javascript/game.js
@@ -9,7 +9,8 @@ var selectedWord = 'hangman';
 var guessesRemaining = 5;
 var wrongGuesses = [];
 var correctGuesses = [];
-var guesses = [];
+var guesses = ["placeHolder"];
+var newLetter = false;
 
 
 //=================================
@@ -30,20 +31,43 @@ function checkGuess(keyPress) {
 //=======================
 
     //Check if letter has already been guessed
+        for (i = 0; i < guesses.length + 1; i++) {
+            if (keyPress.key == guesses[i]) {//identifies a matching letter and tells user to pick a new one
+                alert("You've already guessed that letter! Please pick a new letter.");
+                newLetter = false;
+                break;
+            } else if (keyPress !== guesses[i] && i == guesses.length) { //identifies that a letter is new and ends loop allowing program to continue to next step "correctness"
+                guesses.push(keyPress.key);
+                newLetter = true;
+                break;
+            } else {} //goes to the next i value because all previously guessed letters have not been checked
+            
+        };
 
-    //See if letter is correct
-        for (i = 0; i < selectedWord.length; i++)
+        //See if letter is correct or incorrect
+        if (newLetter == true) {
 
-            if (selectedWord[i] === keyPress.key) {
-               correctGuesses.push(keyPress.key);
-               console.log(correctGuesses);
+            for (i = 0; i < selectedWord.length + 1; i++) {
+
+                if (keyPress.key === selectedWord[i]) {
+                    correctGuesses.push(keyPress.key);
+                    newLetter = false;
+                    break;
+                } else if (keyPress.key !== selectedWord[i] && i == selectedWord.length) {
+                    newLetter = false;
+                    guessesRemaining = guessesRemaining - 1;
+                    wrongGuesses.push(keyPress.key);
+                    break;
+                } else {}
             };
-    }
-
-
+            console.log("guesses: " + guesses);
+            console.log("correctGuesses: " + correctGuesses);
+            console.log("wrongGuesses: " + wrongGuesses);
+            console.log("remainingGuesses :" + guessesRemaining);
+        }
+}
         //===================
         //Call Functions Here
         //===================
 selectWord();
-
 addEventListener("keyup", checkGuess)
